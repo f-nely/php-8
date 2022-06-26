382ceb8dd4cdc8267504d6140117567d095d51e0
nely de faria <nelly.nf54@gmail.com>
2022-06-26 15:10:02 -0300

    Criando arquivo operadores-matematico.php

---
+++ php-8/operadores-matematico.php
@@ -0,0 +1,9 @@
+<?php
+
+// operadores aritméticos (para execução de cálculos)
+$x = 4 + 2; // adição
+$x = 4 - 2; // subtração
+$x = 4 * 2; // multiplicação
+$x = 4 / 2; // divisão
+$x = 4 % 2; // módulos (restante da divisão)
+$x = 4 ** 2; // exponenciação
