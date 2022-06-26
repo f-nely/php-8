e398487a1b555b1e70d95d22b38b14d88063ed8a
nely de faria <nelly.nf54@gmail.com>
2022-06-26 15:16:47 -0300

    Criando arquivo MyClass.php

---
+++ php-8/MyClass.php
@@ -0,0 +1,27 @@
+<?php
+
+class MyClass
+{
+    private string $name;
+    private string $email;
+
+	public function getName(): string
+	{
+		return $this->name;
+	}
+
+	public function setName(string $name): void
+	{
+		$this->name = $name;
+	}
+
+	public function getEmail(): string
+	{
+		return $this->email;
+	}
+
+	public function setEmail(string $email): void
+	{
+		$this->email = $email;
+	}
+}
