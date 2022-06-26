2227cd78d8c0f23bdc4161b8a1982bf1e3e6d74b
nely de faria <nelly.nf54@gmail.com>
2022-06-26 15:06:57 -0300

    Criando arquivo variaveis.php

---
+++ php-8/variaveis.php
@@ -0,0 +1,14 @@
+<?php
+
+$var1 = 0;
+$var_um = 0;
+$varUm = 0;
+$_um = 0;
+$_1 = 0;
+
+echo gettype($_1);
+
+// convenções
+$umaVariavel = 0; // camel case
+$uma_variavel = 0; // com underscores (snake case)
+$UmaVariavel = 0; // studly case (não recomendado)
