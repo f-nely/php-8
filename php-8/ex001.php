17b2f6b72dcbd9c0a9c898a1cfafae61583a2dc7
nely de faria <nelly.nf54@gmail.com>
2022-06-26 15:07:41 -0300

    Criando arquivo ex001.php

---
+++ php-8/ex001.php
@@ -0,0 +1,19 @@
+<?php
+
+$a = 1000;
+$b = $a / 2;
+echo $b . '<br>';
+
+$a = 100;
+$b = $a * 3;
+echo $b . '<br>';
+
+echo $a = 20;
+echo '<br>';
+echo ++$a . '<br>';
+
+$a = 100;
+$b = 250;
+$c = $a + $b;
+$c++;
+echo $c;
