49f1c97bdd31ccd9b772a0f8dede091ddba09f92
nely de faria <nelly.nf54@gmail.com>
2022-06-26 15:16:04 -0300

    Criando arquivo ex004.py

---
+++ php-8/ex004.php
@@ -0,0 +1,8 @@
+<?php
+
+$firstName = 'Taylor';
+$lastName = 'Otwell';
+$firstName = strtoupper($firstName);
+$lastName = strtoupper($lastName);
+
+echo "<p>O meu nome é {$firstName} e o meu sobrenome é {$lastName}</p>";
