d89a20bb91c169be4688097d546ab9da6e69d1a5
nely de faria <nelly.nf54@gmail.com>
2022-06-26 15:12:49 -0300

    Criando arquivo caracteres-escape.php

---
+++ php-8/caracteres-escape.php
@@ -0,0 +1,14 @@
+<?php
+
+$frase1 = 'I\'m going to John\'s Party!';
+echo $frase1 . '<br>';
+
+$frase2 = 'I\'m not sure John "The great" will win this fight.';
+echo $frase2 . '<br>';
+
+$frase3 = "I'm not sure John \"The great\" will win this fight.";
+echo $frase3 . '<br>';
+
+// HTML entities
+echo '&copy';
+
